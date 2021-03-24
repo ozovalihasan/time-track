2742ce39bce982dc33a67dece4ca88eb2c62e490
Hasan Ozovali <ozovalihasan@gmail.com>
2021-03-24 20:23:50 +0300

    Update shown tasks to admins

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,7 +2,7 @@ class TasksController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @tasks = Task.all
+    return @tasks = Task.all if current_user.admin?
   end
 
   def new
