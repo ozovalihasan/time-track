bdb3b73fba5ac0ada8b8c07f77fa1f8bd200046a
Hasan Ozovali <ozovalihasan@gmail.com>
2021-03-28 19:32:32 +0300

    Remove unused csv method

--- app/models/task.rb
+++ app/models/task.rb
@@ -6,16 +6,4 @@ class Task < ApplicationRecord
   scope :filter_by_time_type, ->(time_type) { where time_type: time_type }
   scope :filter_by_start_time, ->(start_time) { where ['start_time > ?', start_time] }
   scope :filter_by_end_time, ->(end_time) { where ['end_time < ?', end_time] }
-
-  def self.to_csv
-    attributes = %w[id comment time_type start_time end_time user_id]
-
-    CSV.generate(headers: true) do |csv|
-      csv << attributes
-
-      all.each do |user|
-        csv << attributes.map { |attr| user.send(attr) }
-      end
-    end
-  end
 end
